c5f052d42050fce604ff54128c0ccb0227896390
Jaci-Xavier <devxvr@gmail.com>
2023-07-13 20:45:25 -0300

    req 02: criando model user

---
+++ src/models/User.js
@@ -0,0 +1,18 @@
+const UserModel = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+      allowNull: false,
+    },
+    displayName: DataTypes.STRING,
+    email: DataTypes.STRING,
+    password: DataTypes.STRING,
+    image: DataTypes.STRING,
+  },{
+    underscore: true,
+    tableName: 'User',
+    timestamps: false,
+  });
+};
